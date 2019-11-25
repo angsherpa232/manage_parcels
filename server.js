25ab1b49a0bc2f9787dc36d3d37f2ffdb26a3bf9
angsherpa232 <angsherpa232@gmail.com>
2019-11-25 18:13:10 +0100

    comments updated in server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Declare variables and constants
 const express = require("express");
 const basicAuth = require("express-basic-auth");
 const cookieParser = require("cookie-parser");
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 // A random key for signing the cookie
 app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
 
+// Declare static folders for the server. Hence build process is mandatory
 app
   .use(express.static(path.join(__dirname, "./build")))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -29,6 +31,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
+// Checks authentication
 app.get("/authenticate", auth, (req, res) => {
   const options = {
     httpOnly: true,
@@ -42,6 +45,7 @@ app.get("/authenticate", auth, (req, res) => {
   }
 });
 
+// Check if user is already logged-in
 app.get("/read-cookie", (req, res) => {
   if (req.signedCookies.name === "john") {
     res.send({ screen: "manager", name: "john" });
@@ -52,10 +56,12 @@ app.get("/read-cookie", (req, res) => {
   }
 });
 
+// Check if user has logged-out
 app.get("/clear-cookie", (req, res) => {
   res.clearCookie("name").end();
 });
 
+// Sends the data to client
 app.get("/get-data", (req, res) => {
   res.json(parcelArray);
 });
